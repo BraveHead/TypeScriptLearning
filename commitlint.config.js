14fd741c64f0db9f0f366e337d30570160644106
zhangshun <zhangshun@supcon.com>
2019-01-09 19:27:41 +0800

    修改；

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'subject-case': [0]
+    'type-enum': [2, 'always', [
+      "feat", "fix", "docs", "style", "refactor", "test", "chore", "revert"
+    ]],
+    'subject-full-stop': [0, 'never'],
+    'subject-case': [0, 'never']
   }
-};
+}
